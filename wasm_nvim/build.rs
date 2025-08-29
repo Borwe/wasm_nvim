7dbc8a0f4f03f8318983fc7d0d82192ac072f38e
brian.orwe <brian.orwe@gmail.com>
2025-08-29 15:50:51 +0300

    ci: Fix Windows test compile

--- wasm_nvim/build.rs
+++ wasm_nvim/build.rs
@@ -89,6 +89,7 @@ async fn get_luajit_source()-> Result<()>{
     println!("cargo:rustc-env=LUA_LIB={:?}",
              src_dir);
     println!("cargo:rustc-env=LUA_LIB_NAME=lua51");
+    println!("cargo:rustc-env=LUA_LINK=dylib");
 
     Ok(())
 }
